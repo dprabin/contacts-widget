4cd3a21dc9ddca94b254d922e1979095240e9ea2
dprabin <dprabin@yahoo.com>
2015-02-02 16:52:04 +0545

    debugged contacts-widget

--- contacts-widget.php
+++ contacts-widget.php
@@ -17,12 +17,12 @@ function add_scripts(){
 	wp_enqueue_script('contact-scripts', plugins_url().'contacts-widget/js/script.js',array('jquery'),'1.0.0',false);
 }
 //load our function
-add_action('wp_enqueue_scripts','add_scripts')
+add_action('wp_enqueue_scripts','add_scripts');
 
 /*
  * Include Class
  */
-include ('class.contacts-widget.php');
+include('class.contacts-widget.php');
 
 /*
  * Register Widget
