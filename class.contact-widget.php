e9b3f429bd779997eb95b1c098bdbdfa99f4f6e9
dprabin <dprabin@yahoo.com>
2015-02-02 16:14:48 +0545

    main widget function with title display

--- class.contact-widget.php
+++ class.contact-widget.php
@@ -11,4 +11,19 @@ class Contacts_Widget extends WP_Widget {
 		);
 	}
 
+	/*
+	 * Frontend Display
+	 */
+	public function widget($args, $instance){
+		$title = apply_filters('widget_title',$instance['title']);
+		$recipient = $instance['recipient'];
+		$subject = $instance['subject'];
+
+		echo $args['before_widget'];
+		if (!empty($title))
+			echo $args['before_title'] . $title . $args['after_title'];
+		
+		echo $args['after_widget'];
+	}
+
 }
