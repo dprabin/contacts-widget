ff63c36bbb358fda6e94defc50ad383b1a4220f9
dprabin <dprabin@yahoo.com>
2015-02-02 16:26:41 +0545

    add backend form

--- class.contact-widget.php
+++ class.contact-widget.php
@@ -29,4 +29,34 @@ class Contacts_Widget extends WP_Widget {
 		echo $args['after_widget'];
 	}
 
+
+	/*
+	 * Backend Form
+	 */
+	public function form($instance){
+		if(isset($instance['title'])){
+			$title = $instance['title']
+		} else {
+			$title = __('Ajax Contact Widget','text_domain');
+		}
+		$recipient = $instance['recipient'];
+		$subject = $instance['subject'];
+
+?>
+		
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:') ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('recipient'); ?>"><?php _e('Recipient:') ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('recipient'); ?>" name="<?php echo $this->get_field_name('recipient'); type="text" value="<?php echo esc_attr($recipient); ?>" ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('subject'); ?>"><?php _e('Subject:') ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('subject'); ?>" name="<?php echo $this->get_field_name('subject'); ?>" type="text" value="<?php echo esc_attr($subject); ?>" />
+		</p>
+<?php
+	}
+
 }
