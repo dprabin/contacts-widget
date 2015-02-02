e28ee7775dd6aefa3e72aac9ab617644cc8b0897
dprabin <dprabin@yahoo.com>
2015-02-02 15:53:06 +0545

    function to add scripts

--- contact-widget.php
+++ contact-widget.php
@@ -1,13 +1,18 @@
 <?php
 /**
  * Plugin Name: Ajax Contacts Widget
- * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
- * Description: A brief description of the plugin.
- * Version: The plugin's version number. Example: 1.0.0
+ * Description: Simple Ajax powered contact form widget
+ * Version: 1.0
  * Author: Prabhu Bhakta
- * Author URI: http://URI_Of_The_Plugin_Author
+ * Author URI: http://admin.cdpa.edu.np
  * Text Domain: Optional. Plugin's text domain for localization. Example: mytextdomain
  * Domain Path: Optional. Plugin's relative directory path to .mo files. Example: /locale/
  * Network: Optional. Whether the plugin can only be activated network wide. Example: true
- * License: A short license name. Example: GPL2
+ * License: GPL2
  */
+
+
+//Include JavaScript
+function add_scripts(){
+	wp_enqueue_script('contact-scripts', plugins_url().'contacts-widget/js/script.js',array('jquery'),'1.0.0',false);
+}
