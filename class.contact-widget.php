fa3a554019eaed09a8f33997bbcf02194a72a010
dprabin <dprabin@yahoo.com>
2015-02-02 16:08:36 +0545

    constructor defined

--- class.contact-widget.php
+++ class.contact-widget.php
@@ -1,4 +1,14 @@
 <?php
 class Contacts_Widget extends WP_Widget {
-	
+	/*
+	 * Plugin Constructor
+	 */
+	public function __construct(){
+		parent::__construct(
+			'contacts_widget', //Basae ID
+			__('Ajax Contact Widget','text_domain'), //readable name
+			array('description' => __('Ajax Powered contact widget', 'text_domain'))
+		);
+	}
+
 }
