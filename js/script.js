742b7892b7bd84731c4789c9d9036037c54a42d9
dprabin <dprabin@yahoo.com>
2015-02-02 17:16:23 +0545

    get form contact and message with jQuery

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//wordpress works in no conflict mode so we cannot use $(document).
+//instead we use jQuery(document). Other are same
+jQuery(document).ready(function(){
+	//Get Form by id
+	var form  = jQuery('#ajax-contact');
+
+	//Messages
+	var formMessages = jQuery('#form-messages');
+});
