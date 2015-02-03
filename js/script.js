bcd65581fd7e9d709d5bb3ffea91568147c0b48e
dprabin <dprabin@yahoo.com>
2015-02-03 09:22:36 +0545

    If ajax request is done, set form class as success and clear form, otherwise set class error and display message

--- js/script.js
+++ js/script.js
@@ -15,5 +15,36 @@ jQuery(document).ready(function(){
 		console.log('Contact Form Submitted');
 
 		//Serialize data
+		var formData = jQuery(form).serialize();
+
+		//submit with ajax
+		jQuery.ajax({
+			type: 'POST',
+			url: jQuery(form).attr('action'),
+			data: formData
+		}).done(function(response){
+			//Make sure message is success
+			jQuery(formMessages).removeClass('error');
+			jQuery(formMessages).addClass('success');
+
+			//set message text
+			jQuery(formMessages).text(response);
+
+			//Clear form fields
+			jQuery('#name').val('');
+			jQuery('#email').val('');
+			jQuery('#message').val('');
+		}).fail(function(data){
+			//Make sure message is success
+			jQuery(formMessages).removeClass('success');
+			jQuery(formMessages).addClass('error');
+
+			//set message text
+			if(data.responseText != ''){
+				jQuery(formMessages).text(data.responseText)
+			} else {
+				jQuery(formMessages).text('An error occured')
+			}
+		});
 	});
 }) ;
