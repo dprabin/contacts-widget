babbb2b79fa390b0b00d56ca62d25680efacd285
dprabin <dprabin@yahoo.com>
2015-02-03 08:51:11 +0545

    prevent form for default action and log message to console

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
+jQuery(document).ready(function(){
 //wordpress works in no conflict mode so we cannot use $(document).
 //instead we use jQuery(document). Other are same
-jQuery(document).ready(function(){
+
 	//Get Form by id
 	var form  = jQuery('#ajax-contact');
 
-	//Messages
-	var formMessages = jQuery('#form-messages');
-});
+	//get <div> element for Form Messages
+	var formMessages = jQuery('#form-message');
+
+	//Form event handler
+	jQuery(form).submit(function(event){
+		//stop browser from submitting form, because we want to grab it in javascript
+		event.preventDefault();
+		console.log('Contact Form Submitted');
+
+		//Serialize data
+	});
+}) ;
